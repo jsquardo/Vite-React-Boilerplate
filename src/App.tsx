6327afd5244802cb49a2e692c538934029d5a204
Johnny Squardo <johnnysquardo@gmail.com>
2023-01-20 16:59:55 -0500

    Added Inter Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <div className='flex justify-center items-center'>
+      <div className='flex items-center justify-center'>
         <a
           href='https://vitejs.dev'
           target='_blank'
